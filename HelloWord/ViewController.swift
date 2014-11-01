60d47bef8d62dacb670901883cf8102acecddf45
yourtiger <tiger-xu@vip.sina.com>
2014-11-01 11:45:30 +0800

    HelloWord
    First Project by swift

--- HelloWord/ViewController.swift
+++ HelloWord/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myHelloButton:UIButton!
+    	
+    var mylable:UILabel = UILabel(frame:CGRect(x:0,y:0,width:300,height:60))
+    var resetbutton :UIButton = UIButton(frame:CGRect(x:0,y:0,width:250,height:60))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mylable.text="say hello"
+        mylable.textAlignment = NSTextAlignment.Center
+        mylable.textColor = UIColor.blueColor()
+        mylable.center = CGPointMake(160,100)
+        view.addSubview(mylable)
+        
+        resetbutton.setTitle("Reset", forState:UIControlState.Normal)
+        resetbutton.backgroundColor = UIColor.redColor()
+        resetbutton.addTarget(self, action: "ResMeg", forControlEvents: UIControlEvents.TouchUpInside)
+        resetbutton.center = CGPointMake(160,300)
+        view.addSubview(resetbutton)
+        
+        myHelloButton.backgroundColor = UIColor.greenColor()
+        myHelloButton.center = CGPointMake(160, 200)
     }
 
+    @IBAction func sayhello(){
+        mylable.text = "Hello World"
+        mylable.textColor = UIColor.redColor()
+    }
+    
+    func ResMeg(){
+        mylable.text = "Say Hello"
+        mylable.textColor = UIColor.blueColor()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
